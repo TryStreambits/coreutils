c981efa2f0a2c978ed9f88fc4ee797e1fe375301
Joshua Strobl <joshua@stroblindustries.com>
2016-01-25 11:07:10 +0200

    Drop HTML compile options and RemoveComments option from TypeScriptCompilerOptions.

--- structs.go
+++ structs.go
@@ -5,7 +5,6 @@ type ProjectConfig struct {
 	Name         string
 	ContentTypes []string
 	Go           GoCompilerOptions         // Go key maps to GoCompilerOptions struct
-	HTML         HTMLCompilerOptions       // HTML maps to HTMLCompilerOptions struct
 	LESS         LESSCompilerOptions       // LESS key maps to LessCompilerOptions struct
 	TypeScript   TypeScriptCompilerOptions // TypeScript maps to TypeScriptCompilerOptions struct
 	UsesTests    string
@@ -24,16 +23,6 @@ type GoBranchOptions struct {
 	BinaryName       string   // Name of the binary to output (will default to project name)
 	DoInstall        bool     // Set DoInstall as a bool (BinaryName will be ignored) Implies this branch is a go package.
 	SourcesToInclude []string // Sources to include, you can be very selective here
-}
-
-// #endregion
-
-// #region HTML
-
-// Options available for "compile" HTML
-type HTMLCompilerOptions struct {
-	Compress bool // Defaults to true, despite bool defaults to false
-	CopyAll  bool // Defaults to true, copies everything in src/html
 }
 
 // #endregion
@@ -63,7 +52,6 @@ type TypeScriptCompilerOptions struct {
 	LibreJSLicense    string // Define LibreJSLicense as the valid Spdx license that is also valid with LibreJS Gopher / LibreJS
 	MinifyContent     bool   // Defaults to true, compiles using Google closure compiler
 	OutputDeclaration bool   // Defaults to false, like a normal bool type
-	RemoveComments    bool   // Defaults to true
 	Target            string // Defaults to ES5 - ES3 and ES6 should be options via TS
 	UniqueHash        bool   // Define UniqueHash as a bool, determines whether we should append a fragment of the hash of the file to the file name
 	UseLibreJSHeader  bool   // Define LibreJSHeader, adds compliant headers to JS code
