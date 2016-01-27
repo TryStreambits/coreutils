9f10b4a6acc525d89d36109bf0e40f40e2ca780e
Joshua Strobl <joshua@stroblindustries.com>
2016-01-27 23:44:08 +0200

    Implment liveOutput var in ExecCommand, enables redirecting output to os.Stdout or os.Stderr.

--- exec.go
+++ exec.go
@@ -1,16 +1,26 @@
 package codeutilsShared
 
 import (
+	"os"
 	"os/exec"
 	"strings"
 )
 
 // ExecCommand
 // Function for executing a utility with args and returning the stringified output
-func ExecCommand(utility string, args []string) string {
+func ExecCommand(utility string, args []string, liveOutput bool) string {
+	var output []byte
 	runner := exec.Command(utility, args...)
-	output, _ := runner.CombinedOutput() // Combine the output of stderr and stdout
-	return string(output)
+
+	if liveOutput { // If we should immediately output the results of the command
+		runner.Stdout = os.Stdout
+		runner.Stderr = os.Stderr
+		runner.Start()
+	} else { // If we should redirect output to var
+		output, _ = runner.CombinedOutput() // Combine the output of stderr and stdout
+	}
+
+	return string(output[:])
 }
 
 // ExecutableExists
@@ -19,7 +29,7 @@ func ExecutableExists(executableName string) bool {
 	executableExists := true // Default to true
 
 	var emptyFlags []string
-	executableCommandMessage := ExecCommand(executableName, emptyFlags) // Generate an empty call to the executable
+	executableCommandMessage := ExecCommand(executableName, emptyFlags, false) // Generate an empty call to the executable
 
 	if strings.Contains(executableCommandMessage, "executable file not found") { // If executable does not exist
 		executableExists = false
