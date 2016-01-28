8b4c2a50895edda4b3efc8d95a1b685702e3fcc4
Joshua Strobl <joshua@stroblindustries.com>
2016-01-28 18:19:09 +0200

    Implemented FindClosestFile function that attempts to find a relative match to the provided file.

--- io.go
+++ io.go
@@ -5,25 +5,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
-
-// IsDir
-// Checks if the string provided is a directory or not (based on the current working directory)
-func IsDir(path string) bool {
-	var isDir bool
-	currentWorkingDirectory, _ := os.Getwd()
-	fileObject, fileOpenError := os.Open(currentWorkingDirectory + "/" + path) // Open currentDirectory + path
-
-	if fileOpenError == nil { // If there was no error opening the file object
-		fileStatistics, filePathError := fileObject.Stat() // Get any stats
-
-		if filePathError == nil { // If we got the statistics properly
-			isDir = fileStatistics.IsDir() // Set isDir to result from fileStatistics
-		}
-	}
-
-	return isDir
-}
 
 // CopyDirectory
 // This function will copy a directory, sub-directories, and files
@@ -83,6 +66,59 @@ func CopyFile(sourceFile, destinationFile string) error {
 	}
 
 	return copyError
+}
+
+// FindClosestFile
+// This function will return the closest related file to the one provided from a specific path
+// Returns: string (Closest file) or error (such as if path doesn't exist)
+func FindClosestFile(file string) (string, error) {
+	var closestFile string
+	var closestFileError error
+
+	directory := filepath.Dir(file) // Get the directory of the file
+	file = filepath.Base(file) // Change file to only being the base (file name and extension)
+
+	if _, fileReadError := ioutil.ReadFile(directory + file); fileReadError != nil { // If there was a read error for the file
+		fileNoCaps := strings.ToLower(file) // Set fileNoCaps to the file name with no caps
+		fileNoHyphen := strings.Replace(file, "-", "", -1) // Set fileNoHyphen to the file name with no -
+		fileNoCapsNoHyphen := strings.Replace(fileNoCaps, "-", "", -1) // Set fileNoCapsNoHyphen to file name with no caps or -
+
+		fileClosestAttempts := []string{ fileNoCaps, fileNoHyphen, fileNoCapsNoHyphen } // Set fileClosestAttempts to an array of the closest attempts strings
+
+		for _, fileAttemptName := range fileClosestAttempts {
+			_, fileAttemptReadError := ioutil.ReadFile(directory + fileAttemptName) // Attempt to read this file
+
+			if fileAttemptReadError == nil { // If we successfully found a match
+				closestFile = directory + fileAttemptName // Set closest file to this attempt name
+			}
+		}
+
+		if closestFile == "" { // If no matches were found
+			closestFileError = errors.New("No match for " + directory + file + " found.")
+		}
+	} else { // If there was no read error for the file, meaning the closest file is the file itself
+		closestFile = directory + file // Set to original file provided
+	}
+
+	return closestFile, closestFileError
+}
+
+// IsDir
+// Checks if the string provided is a directory or not (based on the current working directory)
+func IsDir(path string) bool {
+	var isDir bool
+	currentWorkingDirectory, _ := os.Getwd()
+	fileObject, fileOpenError := os.Open(currentWorkingDirectory + "/" + path) // Open currentDirectory + path
+
+	if fileOpenError == nil { // If there was no error opening the file object
+		fileStatistics, filePathError := fileObject.Stat() // Get any stats
+
+		if filePathError == nil { // If we got the statistics properly
+			isDir = fileStatistics.IsDir() // Set isDir to result from fileStatistics
+		}
+	}
+
+	return isDir
 }
 
 // WriteOrUpdateFile
