b74a999a2bb18336ef88b6552d8d93a25fb4353d
Joshua Strobl <joshua@stroblindustries.com>
2016-05-18 13:38:03 +0300

    Implement rounds in Sha512Sum func

--- codeutilsShared.go
+++ codeutilsShared.go
@@ -18,8 +18,18 @@ func init() {
 }
 
 // Sha512Sum will create a sha512sum of the string
-func Sha512Sum(content string) string {
-	sha512Hasher := sha512.New()                     // Create a new Hash struct
-	sha512Hasher.Write([]byte(content))              // Write the byte array of the content
-	return hex.EncodeToString(sha512Hasher.Sum(nil)) // Return string encoded sum of sha512sum
+func Sha512Sum(content string, rounds int) string {
+	var hashString string
+
+	sha512Hasher := sha512.New()                           // Create a new Hash struct
+	sha512Hasher.Write([]byte(content))                    // Write the byte array of the content
+	hashString = hex.EncodeToString(sha512Hasher.Sum(nil)) // Return string encoded sum of sha512sum
+
+	if (rounds != 0) && (rounds > 1) { // If we are cycling more than one rounds
+		for currentRound := 0; currentRound < rounds; currentRound++ {
+			hashString = Sha512Sum(hashString, 1) // Rehash the new hashString
+		}
+	}
+
+	return hashString
 }
