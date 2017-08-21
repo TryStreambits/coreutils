c6e5dfe72612ce128a2f0e37951f4d6d3e629d46
Joshua Strobl <joshua@stroblindustries.com>
2017-08-22 00:17:45 +0300

    Revert "Ensure we set the Uid and Gid to run the command at."
    This reverts commit 2a252c71057df52baccb62310ab738e7d1ba47b8.

--- exec.go
+++ exec.go
@@ -3,24 +3,13 @@ package coreutils
 import (
 	"os"
 	"os/exec"
-	"os/user"
-	"strconv"
-	"syscall"
 )
 
 // ExecCommand executes a command with args and returning the stringified output
 func ExecCommand(command string, args []string, redirect bool) string {
 	if ExecutableExists(command) { // If the executable exists
-		currentUser, _ := user.Current()
-
 		var output []byte
 		runner := exec.Command(command, args...)
-
-		uidInt, _ := strconv.Atoi(currentUser.Uid)
-		gidInt, _ := strconv.Atoi(currentUser.Gid)
-
-		runner.SysProcAttr = &syscall.SysProcAttr{}
-		runner.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(uidInt), Gid: uint32(gidInt) }
 
 		if redirect { // If we should redirect output to var
 			output, _ = runner.CombinedOutput() // Combine the output of stderr and stdout
